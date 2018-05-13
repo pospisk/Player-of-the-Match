441f7b50c80c9d25b734a7ab929f1d76c12cba32
Kristian Jeppe Schou Guld <kjsguld@gmail.com>
2018-05-13 23:17:31 +0200

    VotePlayersPage added images and icon added

---
+++ potmquick/PlayeroftheGame/Pages/VotePlayersPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PlayeroftheGame.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class VotePlayersPage : ContentPage
+	{
+		public VotePlayersPage (int matchId, int teamId)
+		{
+			InitializeComponent ();
+		}
+	}
+}
