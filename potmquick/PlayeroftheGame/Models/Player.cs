6c4c8ceb52b257f9d5a8266dbd9b4171c3ec3f71
Kristian Jeppe Schou Guld <kjsguld@gmail.com>
2018-05-14 01:02:57 +0200

    added player class, call cleanup

---
+++ potmquick/PlayeroftheGame/Models/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlayeroftheGame.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int JerseyNumber { get; set; }
+        public string PlayerImage { get; set; }
+    }
+}
--- potmquick/PlayeroftheGame/Pages/VotePlayersPage.xaml.cs
+++ potmquick/PlayeroftheGame/Pages/VotePlayersPage.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
+using PlayeroftheGame.Models;
+using PlayeroftheGame.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +15,22 @@ namespace PlayeroftheGame.Pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class VotePlayersPage : ContentPage
 	{
-		public VotePlayersPage (int matchId, int teamId)
+        private static string apiPath = "http://www.api.potg-dev.org/umbraco/api/Player/getPlayers?parentId=";
+
+        public VotePlayersPage (int matchId, int teamId)
 		{
 			InitializeComponent ();
 		}
-	}
+        public async void GetPlayers(int teamId)
+        {
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetStringAsync(apiPath + teamId);
+
+            var players = JsonConvert.DeserializeObject<List<Player>>(response);
+
+            PlayersListView.ItemsSource = players;
+        }
+
+    }
 }
