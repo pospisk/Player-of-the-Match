afbafd9e9cc5199baecf430b840ef1ce0dc53798
Kristian Pospis <kristian.pospis@gmail.com>
2018-05-13 16:13:29 +0200

    Matches pages & image resources

--- potmquick/PlayeroftheGame/Pages/MatchesPage.xaml.cs
+++ potmquick/PlayeroftheGame/Pages/MatchesPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using PlayeroftheGame.Models;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,13 +16,38 @@ namespace PlayeroftheGame
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MatchesPage : ContentPage
 	{
-		public MatchesPage ()
+	    private SKPaint redFillPaint = new SKPaint()
+	    {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.OrangeRed
+	    };
+
+	    private SKPaint blueFillPaint = new SKPaint()
+	    {
+	        Style = SKPaintStyle.Fill,
+	        Color = SKColors.DarkSlateBlue
+	    };
+
+        public MatchesPage ()
 		{
 			InitializeComponent ();
             GetMatches();
 
-		}
+        }
 
+	    private void OnPaintSample(object sender, SKPaintSurfaceEventArgs e)
+	    {
+	        SKSurface surface = e.Surface;
+	        SKCanvas canvas = surface.Canvas;
+
+	        int width = e.Info.Width;
+	        int height = e.Info.Height;
+
+            canvas.Translate(width / 2, height / 2);
+            canvas.Scale(width / 250f);
+	        canvas.DrawCircle(-50,0, 70, redFillPaint);
+            canvas.DrawCircle(50,0, 70, blueFillPaint);
+	    }
 
 	    public async void GetMatches()
 	    {
@@ -32,5 +59,31 @@ namespace PlayeroftheGame
 
 	        MatchesListView.ItemsSource = matches;
 	    }
-    }
+
+	    public async void GetMatch(int Id)
+	    {
+	        HttpClient client = new HttpClient();
+
+	        var response = await client.GetStringAsync("http://www.api.potg-dev.org/umbraco/api/match/getmatches?parentid=" + Id);
+
+	        var match = JsonConvert.DeserializeObject<List<Match>>(response);
+
+	        //MatchListView.ItemsSource = match;
+	    }
+
+
+
+
+
+        public void ListClicked(object sender, SelectedItemChangedEventArgs e)
+	    {
+	        if (e.SelectedItem == null) return; // don't do anything if we just de-selected the row
+
+	            var poopynumber = (e.SelectedItem as Match).Id ;
+
+
+            ((ListView)sender).SelectedItem = null; // de-select the row
+
+        }
+	}
 }
