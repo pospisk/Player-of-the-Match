9bc8c5e39330cabfbe5001cfaa02c1922750f7f0
Kristian Jeppe Schou Guld <kjsguld@gmail.com>
2018-05-15 01:26:55 +0200

    image sizes fixed

--- potmquick/PlayeroftheGame/Models/Match.cs
+++ potmquick/PlayeroftheGame/Models/Match.cs
@@ -7,10 +7,13 @@ namespace PlayeroftheGame.Models
     public class Match
     {
         public int ClubId { get; set; }
+        public string ClubImagePath { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime MatchDate { get; set; }
         public int OpponentId { get; set; }
+        public string OpponentName { get; set; }
+        public string OpponentClubImagePath { get; set; }
         public int Sponsor { get; set; }
         public int TeamId { get; set; }
     }
